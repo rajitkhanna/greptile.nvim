8cbd5b1c3fada9311539dd2ed27f321b04f3c7eb
rajitkhanna <rajitskhanna@gmail.com>
2024-07-11 16:13:18 -0400

    update file format

--- lua/greptile/init.lua
+++ lua/greptile/init.lua
@@ -1,65 +1,22 @@
-local pickers = require("telescope.pickers")
-local finders = require("telescope.finders")
+local pickers = require "telescope.pickers"
+local finders = require "telescope.finders"
 local config = require("telescope.config").values
-local previewers = require("telescope.previewers")
 
-local log = require("plenary.log")
-log.level = "debug"
+local indexed = function ()
 
-local M = {}
-
--- Function to execute a shell command and capture the output
-function os.capture(cmd)
-	local f = assert(io.popen(cmd, "r"))
-	local s = assert(f:read("*a"))
-	f:close()
-	return s
 end
 
--- Capture the current Git branch
-local current_branch = os.capture("git branch --show-current"):gsub("%s+", "")
-local remote_url = os.capture("git remote get-url origin"):gsub("%s+", "")
-log.debug("Current branch: " .. current_branch)
-log.debug("Remote url: " .. remote_url)
-
-M.semantic_search = function(opts)
-	pickers
-		.new(opts, {
-			finder = finders.new_table({
-
-				results = {
-					{ name = "Yes", value = { 1, 2, 3, 44 } },
-					{ name = "No", value = { 1, 2, 3, 45 } },
-					{ name = "Maybe", value = { 1, 2, 3, 46 } },
-					{ name = "So", value = { 1, 2, 3, 47 } },
-				},
-
-				entry_maker = function(entry)
-					return {
-						value = entry.value,
-						display = entry.name,
-						ordinal = entry.name,
-					}
-				end,
-			}),
-			sorter = config.generic_sorter(opts),
-
-			previewer = previewers.new_buffer_previewer({
-				title = "Preview",
-				define_preview = function(self, entry)
-					vim.api.nvim_buf_set_lines(
-						self.state.bufnr,
-						0,
-						0,
-						false,
-						vim.tbl_flatten({ "Hello", "Everyone", vim.inspect(entry.value) })
-					)
-				end,
-			}),
-		})
-		:find()
+local search_repo = function (prompt)
+   
 end
 
-M.semantic_search()
+local semantic_search = function (opts)
+  pickers.new(opts, {
+    prompt_title: 'Semantic Search',
+    finder = finders.new_table({
+      results = search_repo(opts.prompt),
 
-return M
+    })
+  })
+  
+end
--- lua/telescope/_extensions/greptile.lua
+++ lua/telescope/_extensions/greptile.lua
@@ -1,19 +1,7 @@
-local pickers = require "telescope.pickers"
-local finders = require "telescope.finders"
-local config = require("telescope.config").values
+local greptile = require("greptile")
 
-local is_indexed = function ()
-
-end
-
-local search_repo = function (prompt)
-   
-end
-
-local semantic_search = function (opts)
-  pickers.new(opts, {
-    prompt_title: 'Semantic Search',
-    finder = finders.
-  })
-  
-end
+return require("telescope").register_extension({
+	exports = {
+		semantic_search = greptile.semantic_search,
+	},
+})
