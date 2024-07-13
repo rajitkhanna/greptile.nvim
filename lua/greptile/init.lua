89498477d5e5fb3b9e4670a9382567c5e569edfb
rajitkhanna <rajitskhanna@gmail.com>
2024-07-12 22:20:49 -0400

    all done

--- lua/greptile/init.lua
+++ lua/greptile/init.lua
@@ -8,6 +8,7 @@ if not has_plenary then
 	error("This extension requires plenary.nvim (https://github.com/nvim-lua/plenary.nvim)")
 end
 
+local utils = require("telescope.utils")
 local finders = require("telescope.finders")
 local actions = require("telescope.actions")
 local action_state = require("telescope.actions.state")
@@ -18,6 +19,14 @@ local curl = require("plenary.curl")
 local greptile_api_key = os.getenv("GREPTILE_API_KEY")
 local github_token = os.getenv("GITHUB_TOKEN")
 
+local PROCESSING_STATUS = {
+	NOT_STARTED = 1,
+	IN_PROGRESS = 2,
+	COMPLETED = 3,
+}
+
+local M = {}
+
 local function get_git_info()
 	local branch = vim.fn.system("git rev-parse --abbrev-ref HEAD"):gsub("\n", "")
 	local repo_url = vim.fn.system("git config --get remote.origin.url"):gsub("\n", "")
@@ -25,49 +34,106 @@ local function get_git_info()
 	return branch, repo_name
 end
 
-local function get_file_icon(filename)
-	local ok, devicons = pcall(require, "nvim-web-devicons")
-	if not ok then
-		return ""
+local char_to_hex = function(c)
+	return string.format("%%%02X", string.byte(c))
+end
+
+local function urlencode(url)
+	if url == nil then
+		return
 	end
-	local icon, _ = devicons.get_icon(filename, string.match(filename, "%a+$"), { default = true })
-	return icon and (icon .. " ") or ""
+	url = url:gsub("\n", "\r\n")
+	url = url:gsub("([^%w ])", char_to_hex)
+	url = url:gsub(" ", "+")
+	return url
+end
+
+local indexed = function()
+	local branch, repo_name = get_git_info()
+	local repository_id = "github:" .. branch .. ":" .. repo_name
+
+	local url = "https://api.greptile.com/v2/repositories/" .. urlencode(repository_id)
+	local headers = {
+		["Authorization"] = "Bearer " .. greptile_api_key,
+	}
+
+	local response = curl.get(url, {
+		headers = headers,
+	})
+
+	if response.status ~= 200 then
+		-- log.debug(vim.json.decode(response))
+		return PROCESSING_STATUS.NOT_STARTED
+	elseif vim.json.decode(response.body).status == "completed" then
+		return PROCESSING_STATUS.COMPLETED
+	else
+		return PROCESSING_STATUS.IN_PROGRESS
+	end
 end
 
 local search_repo = function(prompt)
 	local branch, repo_name = get_git_info()
 
-	local url = "https://api.greptile.com/v2/search"
-	local headers = {
-		["Authorization"] = "Bearer " .. greptile_api_key,
-		["Content-Type"] = "application/json",
-		["X-Github-Token"] = github_token,
-	}
+	local status = indexed()
+	if status == PROCESSING_STATUS.NOT_STARTED then
+		local url = "https://api.greptile.com/v2/repositories"
 
-	local body = {
-		["query"] = prompt,
-		["repositories"] = {
-			{
-				["remote"] = "github",
-				["branch"] = branch,
-				["repository"] = repo_name,
+		local headers = {
+			["Authorization"] = "Bearer " .. greptile_api_key,
+			["Content-Type"] = "application/json",
+			["X-Github-Token"] = github_token,
+		}
+		local body = {
+			["remote"] = "github",
+			["branch"] = branch,
+			["repository"] = repo_name,
+		}
+
+		local response = curl.post(url, {
+			body = vim.json.encode(body),
+			headers = headers,
+		})
+
+		if response.status ~= 200 then
+			error(
+				"API request failed with status code: " .. response.status .. "\n" .. vim.json.decode(response).response
+			)
+		end
+	elseif status == PROCESSING_STATUS.IN_PROGRESS then
+		return { "Loading..." }
+	else
+		local url = "https://api.greptile.com/v2/search"
+		local headers = {
+			["Authorization"] = "Bearer " .. greptile_api_key,
+			["Content-Type"] = "application/json",
+			["X-Github-Token"] = github_token,
+		}
+
+		local body = {
+			["query"] = prompt,
+			["repositories"] = {
+				{
+					["remote"] = "github",
+					["branch"] = branch,
+					["repository"] = repo_name,
+				},
 			},
-		},
-	}
+		}
 
-	local response = curl.post(url, {
-		body = vim.json.encode(body),
-		headers = headers,
-	})
+		local response = curl.post(url, {
+			body = vim.json.encode(body),
+			headers = headers,
+		})
 
-	if response.status ~= 200 then
-		error("API request failed with status code: " .. response.status)
+		if response.status ~= 200 then
+			error("API request failed with status code: " .. response.status)
+		end
+
+		return vim.json.decode(response.body)
 	end
-
-	return vim.json.decode(response.body)
 end
 
-local semantic_search_picker = function(opts)
+M.semantic_search = function(opts)
 	opts = opts or {}
 	local cwd = vim.fn.getcwd()
 
@@ -80,11 +146,22 @@ local semantic_search_picker = function(opts)
 			finders.new_table({
 				results = results,
 				entry_maker = function(entry)
-					return {
-						value = cwd .. "/" .. entry.filepath,
-						display = get_file_icon(entry.filepath) .. entry.filepath:gsub("^/", ""),
-						ordinal = prompt,
-					}
+					if entry.filepath == nil then
+						return {
+							value = "Loading...",
+							display = "Loading...",
+							ordinal = prompt,
+						}
+					else
+						return {
+							value = cwd .. "/" .. entry.filepath,
+							display = utils.get_devicons(entry.filepath)
+								.. " "
+								.. utils.transform_path(opts, entry.filepath),
+							ordinal = prompt,
+							filepath = entry.filepath,
+						}
+					end
 				end,
 			}),
 			{ reset_prompt = false }
@@ -94,9 +171,8 @@ local semantic_search_picker = function(opts)
 	local function open_selected_file(prompt_bufnr)
 		local selection = action_state.get_selected_entry()
 		if selection and selection.value then
-			local filepath = cwd .. "/" .. selection.value
 			actions.close(prompt_bufnr)
-			vim.cmd("edit " .. filepath)
+			vim.cmd("edit " .. selection.value)
 		else
 			print("No selection or selection value available")
 		end
@@ -108,13 +184,13 @@ local semantic_search_picker = function(opts)
 			map("i", "<CR>", function()
 				refresh_picker_with_results(prompt_bufnr)
 			end)
-			map("i", "<C-e>", function()
+			map("i", "<C-o>", function()
 				open_selected_file(prompt_bufnr)
 			end)
 			map("n", "<CR>", function()
 				refresh_picker_with_results(prompt_bufnr)
 			end)
-			map("n", "<C-e>", function()
+			map("n", "<C-o>", function()
 				open_selected_file(prompt_bufnr)
 			end)
 			return true
@@ -122,4 +198,4 @@ local semantic_search_picker = function(opts)
 	})
 end
 
-semantic_search_picker()
+return M
